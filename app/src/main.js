2b96321dcdb59af8823ad6d0c47cdc9fd91921e7
Wladimir Delenclos <wdelenclos@gmail.com>
2020-06-22 11:19:02 +0200

    fix css file

--- app/src/main.js
+++ app/src/main.js
@@ -5,9 +5,8 @@ import axios from 'axios'
 import store from './store'
 import Services from './api/services'
 
-import './assets/fluance-theme.css'
 import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css'
+import './assets/fluance-theme.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$service = Services
